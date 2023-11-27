e195f2358693911a5e87db1d384c5873b3a95710
Kozma Dávid Dániel <kozma@astron.hu>
2023-11-27 14:55:28 +0100

    Boot animation done;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DesktopStarterComponent } from './components/desktop-starter/desktop-starter.component';
+import { DesktopComponent } from './components/desktop/desktop/desktop.component';
 
 @NgModule({
-	declarations: [AppComponent, DesktopStarterComponent],
-	imports: [BrowserModule, AppRoutingModule],
+	declarations: [AppComponent, DesktopComponent],
+	imports: [BrowserModule, AppRoutingModule, DesktopStarterComponent],
 	providers: [],
 	bootstrap: [AppComponent],
 })
--- src/app/components/desktop-starter/desktop-starter.component.ts
+++ src/app/components/desktop-starter/desktop-starter.component.ts
@@ -1,34 +1,59 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit, HostListener } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-desktop-starter',
 	templateUrl: './desktop-starter.component.html',
 	styleUrls: ['./desktop-starter.component.scss'],
+	standalone: true,
+	imports: [CommonModule],
 })
 export class DesktopStarterComponent implements OnInit {
-	countTime: number = 10;
+	countTimeDesktop: number = 5;
+	countDownDesktopInterval: any;
+
+	countTimeBoot: number = 4;
+	countDownBootInterval: any;
+
 	showBootText: boolean = false;
-	countDownInterval: any;
+
+	constructor(private router: Router) {}
 
 	ngOnInit(): void {
-		this.countDown();
+		this.desktopStarter();
 	}
 
-	countDown(): void {
-		this.countDownInterval = setInterval(() => {
-			this.countTime -= 1;
+	@HostListener('document:keydown.enter')
+	@HostListener('document:touchend', ['$event'])
+	@HostListener('document:click', ['$event'])
+	spaceKeyHit(): void {
+		this.countTimeDesktop = 0;
+		this.showBootText = true;
+		this.bootStarter();
+		clearInterval(this.countDownDesktopInterval);
+	}
 
-			if (this.countTime === 0) {
+	desktopStarter(): void {
+		this.countDownDesktopInterval = setInterval(() => {
+			this.countTimeDesktop -= 1;
+
+			if (this.countTimeDesktop === 0) {
 				this.showBootText = true;
-				clearInterval(this.countDownInterval);
+				this.bootStarter();
+				clearInterval(this.countDownDesktopInterval);
 			}
 		}, 1000);
 	}
 
-	@HostListener('document:keydown.enter')
-	spaceKeyHit(): void {
-		this.countTime = 0;
-		this.showBootText = true;
-		clearInterval(this.countDownInterval);
+	bootStarter(): void {
+		this.countDownBootInterval = setInterval(() => {
+			this.countTimeBoot -= 1;
+
+			if (this.countTimeBoot === 0) {
+				clearInterval(this.countDownBootInterval);
+				this.router.navigate(['/desktop']);
+			}
+		}, 1000);
 	}
 }
